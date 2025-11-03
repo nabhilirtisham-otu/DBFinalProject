d5247b6af7bc49267473ef5116c02b3b59db3c3a
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-03 14:44:13 -0500

    Adding tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -7,5 +7,59 @@ CREATE TABLE User(
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-insert INTO User VALUES(1, 'jim', 'jim@jim.com', 'jim123', 'Customer', '2025-11-03 13:55:30');
-Select * from user;
+CREATE TABLE Organizer(
+    organizer_id INT PRIMARY KEY,
+    FOREIGN KEY (organizer_id) REFERENCES (User.user_id),
+    organization_name VARCHAR(100),
+    phone VARCHAR(20)
+);
+
+CREATE TABLE Event(
+    event_id INT PRIMARY KEY,
+    organizer_id INT,
+    FOREIGN KEY (organizer_id) REFERENCES (Organizer.organizer_id),
+    venue_id INT,
+    FOREIGN KEY (venue_id) REFERENCES (Venue.venue_id),
+    title VARCHAR(150),
+    event_description TEXT,
+    start_time DATETIME,
+    end_time DATETIME,
+    standard_price DECIMAL(8,2),
+    event_status ENUM('scheduled', 'ongoing', 'cancelled', 'completed')
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
+
+CREATE TABLE (
+
+);
