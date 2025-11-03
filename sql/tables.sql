3c1988436e9e33aaf7baa818bb24cafc8126bdc5
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-03 18:23:56 -0500

    Adding tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE User(
 
 CREATE TABLE Organizer(
     organizer_id INT PRIMARY KEY,
-    FOREIGN KEY (organizer_id) REFERENCES (User.user_id),
+    FOREIGN KEY (organizer_id) REFERENCES (User.user_id) ON DELETE CASCADE,
     organization_name VARCHAR(100),
     phone VARCHAR(20)
 );
@@ -17,20 +17,20 @@ CREATE TABLE Organizer(
 CREATE TABLE Event_(
     event_id INT PRIMARY KEY,
     organizer_id INT,
-    FOREIGN KEY (organizer_id) REFERENCES (Organizer.organizer_id),
+    FOREIGN KEY (organizer_id) REFERENCES (Organizer.organizer_id) ON DELETE CASCADE,
     venue_id INT,
-    FOREIGN KEY (venue_id) REFERENCES (Venue.venue_id),
+    FOREIGN KEY (venue_id) REFERENCES (Venue.venue_id) ON DELETE RESTRICT,
     title VARCHAR(150),
     event_description TEXT,
     start_time DATETIME,
     end_time DATETIME,
     standard_price DECIMAL(8,2),
-    event_status ENUM('scheduled', 'ongoing', 'cancelled', 'completed')
+    event_status ENUM('Scheduled', 'Ongoing', 'Cancelled', 'Completed')
 );
 
 CREATE TABLE Venue(
     venue_id INT PRIMARY KEY,
-    name VARCHAR(100),
+    venue_name VARCHAR(100),
     loc_address VARCHAR(200),
     city VARCHAR(50),
     holding_capacity INT
@@ -39,39 +39,61 @@ CREATE TABLE Venue(
 CREATE TABLE Section(
     section_id INT PRIMARY KEY,
     venue_id INT,
-    FOREIGN KEY (venue_id) REFERENCES (Venue.venue_id),
-    name VARCHAR(40),
+    FOREIGN KEY (venue_id) REFERENCES (Venue.venue_id) ON DELETE CASCADE,
+    section_name VARCHAR(40),
     seating_capacity INT
 );
 
 CREATE TABLE Seat(
     seat_id INT PRIMARY KEY,
-    section_id INT
-    FOREIGN KEY (section_id) REFERENCES (Section.section_id),
+    section_id INT,
+    FOREIGN KEY (section_id) REFERENCES (Section.section_id) ON DELETE CASCADE,
     seat_number VARCHAR(10),
     row_number VARCHAR(10)
 );
 
-CREATE TABLE (
-
+CREATE TABLE Ticket(
+    ticket_id INT PRIMARY KEY,
+    event_id INT,
+    FOREIGN KEY (event_id) REFERENCES (Event_.event_id) ON DELETE CASCADE,
+    seat_id INT,
+    FOREIGN KEY (seat_id) REFERENCES (Seat.seat_id) ON DELETE CASCADE,
+    ticket_price DECIMAL(8,2),
+    ticket_status ENUM('Available', 'Reserved', 'Sold'),
+    listed_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE (
-
+CREATE TABLE Order(
+    order_id INT PRIMARY KEY,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES (User.user_id) ON DELETE CASCADE,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    order_amount DECIMAL(10,2),
+    order_status ENUM('Pending', 'Paid', 'Refunded', 'Cancelled')
 );
 
-CREATE TABLE (
-
+CREATE TABLE Payment(
+    payment_id INT PRIMARY KEY,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES (Order.order_id) ON DELETE CASCADE,
+    payment_method ENUM('Credit', 'Debit', 'PayPal', 'Cash'),
+    payment_amount DECIMAL(10,2),
+    payment_date TIMESTAMP,
+    payment_status ENUM('Completed', 'Failed', 'Refunded')
 );
 
-CREATE TABLE (
-
+CREATE TABLE Performer(
+    performer_id INT PRIMARY KEY,
+    performer_name VARCHAR(70),
+    genre VARCHAR(80)
 );
 
-CREATE TABLE (
-
+CREATE TABLE Category(
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(50),
+    category_description TEXT
 );
 
-CREATE TABLE (
-
+CREATE TABLE Notif(
+    notification_id INT PRIMARY KEY
 );
