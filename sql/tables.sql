db13a7be2eada2dcb078aa974fd4ede9d47e3319
Nabhil Irtisham <nabhil.irtisham@gmail.com>
2025-11-03 23:57:13 -0500

    Minor table edits

--- sql/tables.sql
+++ sql/tables.sql
@@ -9,9 +9,9 @@ CREATE TABLE Users(
 
 CREATE TABLE Organizer(
     organizer_id INT PRIMARY KEY,
-    FOREIGN KEY (organizer_id) REFERENCES Users(users_id) ON DELETE CASCADE,
     organization_name VARCHAR(100),
-    phone VARCHAR(20)
+    phone VARCHAR(20),
+    FOREIGN KEY (organizer_id) REFERENCES Users(users_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Venue(
@@ -25,62 +25,62 @@ CREATE TABLE Venue(
 CREATE TABLE Event_(
     event_id INT PRIMARY KEY AUTO_INCREMENT,
     organizer_id INT NOT NULL,
-    FOREIGN KEY (organizer_id) REFERENCES Organizer(organizer_id) ON DELETE CASCADE,
     venue_id INT NOT NULL,
-    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id) ON DELETE RESTRICT,
     title VARCHAR(150) NOT NULL,
     event_description TEXT,
     start_time DATETIME,
     end_time DATETIME,
     standard_price DECIMAL(8,2) CHECK (standard_price >= 0),
-    event_status ENUM('Scheduled', 'Ongoing', 'Cancelled', 'Completed') DEFAULT 'Scheduled'
+    event_status ENUM('Scheduled', 'Ongoing', 'Cancelled', 'Completed') DEFAULT 'Scheduled',
+    FOREIGN KEY (organizer_id) REFERENCES Organizer(organizer_id) ON DELETE CASCADE,
+    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id) ON DELETE RESTRICT
 );
 
 CREATE TABLE Section(
     section_id INT PRIMARY KEY AUTO_INCREMENT,
     venue_id INT NOT NULL,
-    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id) ON DELETE CASCADE,
     section_name VARCHAR(40),
-    seating_capacity INT CHECK (seating_capacity >= 0)
+    seating_capacity INT CHECK (seating_capacity >= 0),
+    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Seat(
     seat_id INT PRIMARY KEY AUTO_INCREMENT,
     section_id INT NOT NULL,
-    FOREIGN KEY (section_id) REFERENCES Section(section_id) ON DELETE CASCADE,
     seat_number VARCHAR(10),
-    row_num VARCHAR(10)
+    row_num VARCHAR(10),
+    FOREIGN KEY (section_id) REFERENCES Section(section_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Ticket(
     ticket_id INT PRIMARY KEY AUTO_INCREMENT,
     event_id INT NOT NULL,
-    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
     seat_id INT NOT NULL,
-    FOREIGN KEY (seat_id) REFERENCES Seat(seat_id) ON DELETE CASCADE,
     ticket_price DECIMAL(8,2) CHECK (ticket_price >= 0),
     ticket_status ENUM('Available', 'Reserved', 'Sold') DEFAULT 'Available',
     listed_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (event_id, seat_id)
+    UNIQUE (event_id, seat_id),
+    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
+    FOREIGN KEY (seat_id) REFERENCES Seat(seat_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     users_id INT NOT NULL,
-    FOREIGN KEY (users_id) REFERENCES Users(users_id) ON DELETE CASCADE,
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     order_amount DECIMAL(10,2) CHECK (order_amount >= 0),
-    order_status ENUM('Pending', 'Paid', 'Refunded', 'Cancelled') DEFAULT 'Pending'
+    order_status ENUM('Pending', 'Paid', 'Refunded', 'Cancelled') DEFAULT 'Pending',
+    FOREIGN KEY (users_id) REFERENCES Users(users_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Payment(
     payment_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT NOT NULL UNIQUE,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
     payment_method ENUM('Credit', 'Debit', 'PayPal', 'Cash') NOT NULL,
     payment_amount DECIMAL(10,2) CHECK (payment_amount >= 0),
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    payment_status ENUM('Completed', 'Failed', 'Refunded') DEFAULT 'Completed'
+    payment_status ENUM('Completed', 'Failed', 'Refunded') DEFAULT 'Completed',
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Performer(
@@ -98,26 +98,26 @@ CREATE TABLE Category(
 CREATE TABLE Notif(
     notification_id INT PRIMARY KEY AUTO_INCREMENT,
     users_id INT NOT NULL,
-    FOREIGN KEY (users_id) REFERENCES Users(users_id) ON DELETE CASCADE,
     event_id INT NOT NULL,
-    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
     notification_message TEXT,
     date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    notification_status ENUM('Unread', 'Read') DEFAULT 'Unread'
+    notification_status ENUM('Unread', 'Read') DEFAULT 'Unread',
+    FOREIGN KEY (users_id) REFERENCES Users(users_id) ON DELETE CASCADE
+    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
 );
 
 CREATE TABLE Event_Performer(
     event_id INT,
-    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
     performer_id INT,
-    FOREIGN KEY (performer_id) REFERENCES Performer(performer_id) ON DELETE CASCADE,
-    PRIMARY KEY(event_id, performer_id)
+    PRIMARY KEY(event_id, performer_id),
+    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
+    FOREIGN KEY (performer_id) REFERENCES Performer(performer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Event_Category(
     event_id INT,
-    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
     category_id INT,
+    PRIMARY KEY (event_id, category_id),
+    FOREIGN KEY (event_id) REFERENCES Event_(event_id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES Category(category_id) ON DELETE CASCADE,
-    PRIMARY KEY (event_id, category_id)
 );
